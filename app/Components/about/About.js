37a56dfb6de8a93766f0a9adfc7ec394881c12ab
unknown <tatlidilonur0@gmail.com>
2024-06-29 21:13:39 -0400

    portfolio all pushed

--- app/Components/about/About.js
+++ app/Components/about/About.js
@@ -15,7 +15,8 @@ const About = () => {
         <div className='about__data'>
           <div className='about__info'>
             <p className='about__description'>
-            My journey began with a Bachelor's in Geological Engineering from Istanbul Technical University, where I developed analytical and problem-solving skills. Transitioning to tech, I pursued further education and gained practical experience in modern web technologies like HTML, CSS, JavaScript, React, and Next.js. I have diverse experience in IT, consultancy, and web development in both the USA and Turkey. My goal is to create impactful, user-centric applications and continuously seek opportunities to collaborate on exciting projects. Feel free to reach out if you're looking for a dedicated and skilled developer!</p>
+              My journey began with a Bachelor&apos;s in Geological Engineering from Istanbul Technical University, where I developed analytical and problem-solving skills. Transitioning to tech, I pursued further education and gained practical experience in modern web technologies like HTML, CSS, JavaScript, React, and Next.js. I have diverse experience in IT, consultancy, and web development in both the USA and Turkey. My goal is to create impactful, user-centric applications and continuously seek opportunities to collaborate on exciting projects. Feel free to reach out if you&apos;re looking for a dedicated and skilled developer!
+            </p>
           </div>
         </div>
       </div>
--- app/Components/home/Home.js
+++ app/Components/home/Home.js
@@ -7,22 +7,21 @@ import Shapes from './Shapes';
 const Home = () => {
   return (
    <section className='home container' id='home'>
-   <div className='intro'>
-    <img src= "https://media.licdn.com/dms/image/D5603AQHAMDZS5GUfMw/profile-displayphoto-shrink_200_200/0/1699489895020?e=1724889600&v=beta&t=fS9LtPKj6yfol0fmAb9v0BXJHNmhepLlNWdt2FIOVRM" height={145} width={160} alt='Onur Tatlıdil' className='home__img'/>
-    <h1 className='home__name'>Onur Tatlıdil</h1>
-    <span className='home__education'>
-    MSCS Student & Web Developer
-    </span>
+     <div className='intro'>
+       <img src="https://media.licdn.com/dms/image/D5603AQHAMDZS5GUfMw/profile-displayphoto-shrink_200_200/0/1699489895020?e=1724889600&v=beta&t=fS9LtPKj6yfol0fmAb9v0BXJHNmhepLlNWdt2FIOVRM" height={145} width={160} alt='Onur Tatlıdil' className='home__img'/>
+       <h1 className='home__name'>Onur Tatlıdil</h1>
+       <span className='home__education'>
+         MSCS Student & Web Developer
+       </span>
 
-    <HeaderSocials/>
+       <HeaderSocials/>
 
-    <a href='https://drive.google.com/file/d/1RTHQWkxhVWzl__ogCAZT8A0sCW08ICD-/view' className='btn' download={"Onur's Resume"}>Download Onur's Resume</a>
+       <a href='https://drive.google.com/file/d/1RTHQWkxhVWzl__ogCAZT8A0sCW08ICD-/view' className='btn' download={"Onur&apos;s Resume"}>Download Onur&apos;s Resume</a>
 
-    <ScrollDown/>
-   </div>
+       <ScrollDown/>
+     </div>
 
-   <Shapes/>
-
+     <Shapes/>
    </section> 
   )
 }
